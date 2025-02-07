e193ae481653e9f93d6c04805866672f8af7c017
BugHunter-X <askzenai@gmail.com>
2025-02-07 08:38:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,1 @@
+function foo(a,b){    if (a === 0 && b === 0) {        return 0;    }    return a + b;}console.log(foo(0,1));// expected 1, got 1console.log(foo(1,0));// expected 1, got 1console.log(foo(0,0));// expected 0, got 0
